3ab5a89b8b67aee8f9499eff34e90b9d38ec62f2
whuang45 <=>
2026-02-07 14:31:27 -0800

    downloaded java driver and added string uri

--- backend/src/main/java/com/sessions/SessionsApplication.java
+++ backend/src/main/java/com/sessions/SessionsApplication.java
@@ -1,11 +1,31 @@
 package com.sessions;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import static com.mongodb.client.model.Filters.eq;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SessionsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SessionsApplication.class, args);
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://whuang45_db_user:clijlassMZeEPzqm@hackthecoast.hgra0ow.mongodb.net/?appName=HacktheCoast";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
     }
 }
