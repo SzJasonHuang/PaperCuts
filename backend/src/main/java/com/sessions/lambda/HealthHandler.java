3e3b3b9767091a9a3649496eb50e541c47ec2881
Jason Huang <jhszno1@gmail.com>
2026-02-25 00:13:20 -0800

    feat/simple lambda server health checker

---
+++ backend/src/main/java/com/sessions/lambda/HealthHandler.java
@@ -0,0 +1,19 @@
+package com.sessions.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class HealthHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>{
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(
+        APIGatewayProxyRequestEvent request,
+        Context context) {
+
+    return new APIGatewayProxyResponseEvent()
+            .withStatusCode(200)
+            .withBody("{\"status\":\"healthy\"}");
+    }
+    
+}
